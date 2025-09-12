8ecae6f670b2c7315e8ccc764297d61d2a36f78f
Éric Spérano <eric.sperano@gmail.com>
2025-09-11 19:36:32 -0700

    wip

---
+++ BRIQ-ios/BRIQ_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  BRIQ_iosApp.swift
+//  BRIQ-ios
+//
+//  Created by Éric Spérano on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BRIQ_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BRIQ-macos/BRIQ_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  BRIQ_macosApp.swift
+//  BRIQ-macos
+//
+//  Created by Éric Spérano on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BRIQ_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
